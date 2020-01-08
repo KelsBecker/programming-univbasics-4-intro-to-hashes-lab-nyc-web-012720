6575a5309c9932a981ed33d43114d9cb10b594ba
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-08 19:16:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,24 +1,34 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
-def my_hash
   # return a valid hash with any key/value pair of your choice
+def my_hash
+  who_am_i = {
+    name: "Kelly",
+    age: "31",
+    home: "NYC"
+  }
 end
+
 
 def pioneer
+  {name:"Grace Hopper"}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  {id: 5 }
 end
 
 def my_hash_creator(key, value)
+  {key => value}
   # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
+  hash[key]
   # return the correct value using the hash and key parameters
 end
 
